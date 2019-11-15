3a44d21f367c4e3697d5663e1942247fd4395561
Justin Kofskie <justinkofskie@yahoo.com>
2019-11-15 04:08:20 -0500

    New menu script

--- scripts/hoverbehavior.js
+++ scripts/hoverbehavior.js
@@ -1,27 +1,129 @@
 $(function () {
+
+
+
+
+
+
+
+    let start;
+    let elapsed = 0;
+    let rafHandle;
+
+    function hoverAnimationOak() {
+        if (elapsed <= 100) {
+            elapsed = new Date().getTime() - start;
+            let val = elapsed / 100;
+
+            $('.selector-block').css('grid-template-columns', `${1.0 + val}fr 1fr 1fr 1fr`);
+            //console.log(1 + val);
+            //console.log(elapsed);
+            window.setTimeout(function () {
+                rafHandle = window.requestAnimationFrame(hoverAnimationOak);
+            }, 100 / 60); // about 60 FPS
+        }
+    }
+
     $('#select-oak').hover(function () {
-        $('.selector-block').css('grid-template-columns', '2fr 1fr 1fr 1fr');
+        start = new Date().getTime();
+        elapsed = 0;
+        rafHandle = window.requestAnimationFrame(hoverAnimationOak);
     }, function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 1fr 1fr');
-    });
+        // handle mouse leave animation here instead
+        window.cancelAnimationFrame(rafHandle);
+        elapsed = Infinity;
+        $('.selector-block').css('grid-template-columns', `1fr 1fr 1fr 1fr`);
+    })
+
+
+
+
+
+
+
+
+
+    function hoverAnimationCherry() {
+        if (elapsed <= 100) {
+            elapsed = new Date().getTime() - start;
+            let val = elapsed / 100;
+
+            $('.selector-block').css('grid-template-columns', `1fr ${1.0 + val}fr 1fr 1fr`);
+            //console.log(1 + val);
+            //console.log(elapsed);
+            window.setTimeout(function () {
+                rafHandle = window.requestAnimationFrame(hoverAnimationCherry);
+            }, 100 / 60); // about 60 FPS
+        }
+    }
 
     $('#select-cherry').hover(function () {
-        $('.selector-block').css('grid-template-columns', '1fr 2fr 1fr 1fr');
+        start = new Date().getTime();
+        elapsed = 0;
+        rafHandle = window.requestAnimationFrame(hoverAnimationCherry);
     }, function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 1fr 1fr');
-    });
+        // handle mouse leave animation here instead
+        window.cancelAnimationFrame(rafHandle);
+        elapsed = Infinity;
+        $('.selector-block').css('grid-template-columns', `1fr 1fr 1fr 1fr`);
+    })
+
+
+
+
+    function hoverAnimationSpruce() {
+        if (elapsed <= 100) {
+            elapsed = new Date().getTime() - start;
+            let val = elapsed / 100;
+
+            $('.selector-block').css('grid-template-columns', `1fr 1fr ${1.0 + val}fr 1fr`);
+            //console.log(1 + val);
+            //console.log(elapsed);
+            window.setTimeout(function () {
+                rafHandle = window.requestAnimationFrame(hoverAnimationSpruce);
+            }, 100 / 60); // about 60 FPS
+        }
+    }
 
     $('#select-spruce').hover(function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 2fr 1fr');
+        start = new Date().getTime();
+        elapsed = 0;
+        rafHandle = window.requestAnimationFrame(hoverAnimationSpruce);
     }, function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 1fr 1fr');
-    });
+        // handle mouse leave animation here instead
+        window.cancelAnimationFrame(rafHandle);
+        elapsed = Infinity;
+        $('.selector-block').css('grid-template-columns', `1fr 1fr 1fr 1fr`);
+    })
+
+
+
+
+    function hoverAnimationPine() {
+        if (elapsed <= 100) {
+            elapsed = new Date().getTime() - start;
+            let val = elapsed / 100;
+
+            $('.selector-block').css('grid-template-columns', `1fr 1fr 1fr ${1.0 + val}fr`);
+            //console.log(1 + val);
+            //console.log(elapsed);
+            window.setTimeout(function () {
+                rafHandle = window.requestAnimationFrame(hoverAnimationPine);
+            }, 100 / 60); // about 60 FPS
+        }
+    }
 
     $('#select-pine').hover(function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 1fr 2fr');
+        start = new Date().getTime();
+        elapsed = 0;
+        rafHandle = window.requestAnimationFrame(hoverAnimationPine);
     }, function () {
-        $('.selector-block').css('grid-template-columns', '1fr 1fr 1fr 1fr');
-    });
+        // handle mouse leave animation here instead
+        window.cancelAnimationFrame(rafHandle);
+        elapsed = Infinity;
+        $('.selector-block').css('grid-template-columns', `1fr 1fr 1fr 1fr`);
+    })
+
 
     var isSelected = false;
 
@@ -45,22 +147,5 @@ $(function () {
             isSelected = true;
         }
     });
-
-    /*     $(document).click(function (e) {
-        if (!$(".info-window").is(e.target) && $(".info-window").has(e.target).length == 0 && isSelected == true) {
-            $(".info-window").hide();
-
-            $("body :not(.info-window):not(.background-head)").css("filter", "none");
-            $(".background-head").css("filter", "none");
-            isSelected = false;
-
-        } else if ($("#select-oak").is(e.target)) {
-            $(".info-window").css("display", "block");
-
-            $("body :not(.info-window):not(.selector-tag):not()").css("filter", "blur(5px)");
-            $("#selector-tag-text").css("filter", "blur(5px)");
-            isSelected = true;
-        }
-    }); */
 
 })
