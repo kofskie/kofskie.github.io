c62689aa4ede56c0fd0d2d635e0dbd9343e6a4b0
Justin Kofskie <justinkofskie@yahoo.com>
2019-12-29 15:58:12 -0500

    Fixes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     // sets main header position to random
     var title = $('.about-section__header');
@@ -7,10 +7,19 @@ $(function() {
     var yIndex = Math.floor(Math.random() * 4);
 
     var leftMargs = ['20%', '25%', '30%', '35%'];
-    var topMargs = ['25%', '30%', '35%', '40%'];
+    var topMargs = ['20%', '25%', '30%', '35%'];
 
-    title.css({'left': leftMargs[xIndex], 
-    'top': topMargs[yIndex], 
-    'display': 'inline'});
+    title.css({
+        'left': leftMargs[xIndex],
+        'top': topMargs[yIndex],
+        'display': 'inline'
+    });
+
+    $('.section').css({
+        'left': '0',
+        'top': '0',
+        'margin-left': '0',
+        'margin-top': '0'
+    })
 
 })
