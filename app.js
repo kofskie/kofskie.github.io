18f2a0876a503efc36e1e704504e9e73d3312a3e
Justin Kofskie <justinkofskie@yahoo.com>
2019-12-29 18:35:53 -0500

    Header text

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 $(function () {
 
-    // sets main header position to random
-    var title = $('.about-section__header');
+
+    var title = $('.about-section__container');
 
     var xIndex = Math.floor(Math.random() * 4);
     var yIndex = Math.floor(Math.random() * 4);
 
     var leftMargs = ['20%', '25%', '30%', '35%'];
-    var topMargs = ['20%', '25%', '30%', '35%'];
+    var topMargs = ['25%', '30%', '35%', '40%'];
 
     title.css({
         'left': leftMargs[xIndex],
         'top': topMargs[yIndex],
-        'display': 'inline'
+        'visibility': 'visible'
     });
 
     $('.section').css({
         'left': '0',
-        'top': '0',
-        'margin-left': '0',
-        'margin-top': '0'
+        'top': '0'
     })
 
 })
