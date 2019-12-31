2bca75f272316dce7ae5e0db5aaf49c715ea2ecd
Justin Kofskie <justinkofskie@yahoo.com>
2019-12-31 13:24:49 -0500

    About page text block

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ $(function () {
 
 
     var title = $('.about-section__container');
+    var textContainer = $('.about-section__para-container');
 
     var xIndex = Math.floor(Math.random() * 4);
     var yIndex = Math.floor(Math.random() * 4);
 
-    var leftMargs = ['20%', '25%', '30%', '35%'];
+    var leftMargs = [15, 20, 25, 30];
     var topMargs = ['25%', '30%', '35%', '40%'];
 
     title.css({
-        'left': leftMargs[xIndex],
+        'left': `${leftMargs[xIndex]}%`,
         'top': topMargs[yIndex],
         'visibility': 'visible'
     });
-
-    $('.section').css({
-        'left': '0',
-        'top': '0'
+    var textContainerWidth = 105 - leftMargs[xIndex];
+    textContainer.css({
+        'width': `${textContainerWidth}%`,
+        'visibility': 'visible',
+        'animation': 'header-opac-in 0.2s forwards'
     })
+
 
 })
