39d2ce7a4a52f6a715f3e190f925f6b19f4db2f3
Justin Kofskie <justinkofskie@yahoo.com>
2020-02-01 14:03:00 -0500

    Slight structure changes

--- menus.js
+++ menus.js
@@ -1,5 +1,22 @@
 $(function () {
 
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
     $('.resizable').resizable({
         handles: "e"
     });
